0cc3003317cffc3772873d9e78486eb5dda4a3fd
EricHsu406 <ericeric4062@gmail.com>
2023-12-08 17:50:57 +0800

    加入牛頓軟體Json套件，以及天氣預報類別註解

--- WebApiReview/WeatherForecast.cs
+++ WebApiReview/WeatherForecast.cs
@@ -1,13 +1,16 @@
 namespace WebApiReview
 {
     public class WeatherForecast
-    {
+    {   
         public DateOnly Date { get; set; }
 
+        //攝氏溫度
         public int TemperatureC { get; set; }
 
+        //華氏溫度
         public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 
+        //可為null的結論
         public string? Summary { get; set; }
     }
 }
