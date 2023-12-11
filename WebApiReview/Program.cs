63cf5992aaab57cdf6b0d9d0d81b647bf20229b5
EricHsu406 <ericeric4062@gmail.com>
2023-12-11 12:15:05 +0800

    program中文註釋

--- WebApiReview/Program.cs
+++ WebApiReview/Program.cs
@@ -1,13 +1,19 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
+#region 依賴注入
+// Add services to the container.  
 builder.Services.AddControllers();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
+
+// Generate Swagger quick API Testing page
 builder.Services.AddSwaggerGen();
+#endregion
 
 var app = builder.Build();
+
+#region 應用程式管道順序
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -16,10 +22,16 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+// 路由設定
 app.UseHttpsRedirection();
 
+// 啟動API授權管理
 app.UseAuthorization();
 
+// Api mapping API路由映射
 app.MapControllers();
 
+#endregion
+
+// 應用程式啟動
 app.Run();
